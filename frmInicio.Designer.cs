b7a1d02d4e2a1268d69869632058bf6d8999ffd5
Francis Gonzalez <Kitomc.rd@gmail.com>
2019-09-19 18:16:59 -0400

    Agregar archivos de proyecto.

---
+++ frmInicio.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Sistema_de_Facturacion_Servicios_Diversos_Juan
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlIzquierdo = new System.Windows.Forms.Panel();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlIzquierdo
+            // 
+            this.pnlIzquierdo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.pnlIzquierdo.Name = "pnlIzquierdo";
+            this.pnlIzquierdo.Size = new System.Drawing.Size(200, 511);
+            this.pnlIzquierdo.TabIndex = 0;
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.Controls.Add(this.label1);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(200, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(504, 43);
+            this.pnlHeader.TabIndex = 1;
+            // 
+            // pnlContenedor
+            // 
+            this.pnlContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContenedor.Location = new System.Drawing.Point(200, 0);
+            this.pnlContenedor.Name = "pnlContenedor";
+            this.pnlContenedor.Size = new System.Drawing.Size(504, 511);
+            this.pnlContenedor.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(466, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 511);
+            this.Controls.Add(this.pnlHeader);
+            this.Controls.Add(this.pnlContenedor);
+            this.Controls.Add(this.pnlIzquierdo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio";
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlIzquierdo;
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlContenedor;
+    }
+}
+
---
+++ frmInicio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Facturacion_Servicios_Diversos_Juan
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
